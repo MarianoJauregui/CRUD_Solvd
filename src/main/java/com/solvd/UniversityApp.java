ca818cb7a77d072b2928bd541ff4ff37b090787c
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-11 15:37:40 -0300

    Connected to database

--- src/main/java/com/solvd/UniversityApp.java
+++ src/main/java/com/solvd/UniversityApp.java
@@ -6,8 +6,7 @@ import java.sql.*;
 public class UniversityApp {
     public static void main( String[] args ){
 
-        // String jdbc = "jdbc:mysql://52.59.193.212:3306/jauregui_mariano";
-        String jdbc = "jdbc:/mysql://localhost:3306/my";
+        String jdbc = "jdbc:mysql://localhost:3306/jauregui_mariano?useSSL=false";
 
         try{
             Connection conexion = DriverManager.getConnection(jdbc, "root", "123456" );
