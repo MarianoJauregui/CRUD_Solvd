36fa258b10e8b1787d0109edfb803ae48d64ef64
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-05-12 15:16:24 -0300

    Connected to database, implementing Marshaller

--- src/main/java/com/solvd/UniversityApp.java
+++ src/main/java/com/solvd/UniversityApp.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+
 import java.sql.*;
 
 
@@ -19,19 +20,26 @@ public class UniversityApp {
         Student student = new Student(1L, "Mariano", "Jauregui", "asd@asd.com");
 
         String jdbc = "jdbc:mysql://localhost:3306/jauregui_mariano?useSSL=false";
+        final String CREATE_TABLE = "CREATE DATABASE Employee";
+        PreparedStatement statement = null;
 
         try{
             Connection conexion = DriverManager.getConnection(jdbc, "root", "123456" );
             System.out.println("Im in.");
+            statement = conexion.prepareStatement(CREATE_TABLE);
+            statement.executeUpdate();
+            System.out.println("Table created");
             conexion.close();
         } catch (SQLException e){
-            e.printStackTrace();
+            LOGGER.error(e);
         }
+
 
         try{
             JAXBContext contextObj = JAXBContext.newInstance(Student.class);
             Marshaller marshallerObj = contextObj.createMarshaller();
             marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            System.out.println(marshallerObj);
         } catch(JAXBException e) {
             LOGGER.error(e.getMessage());
         }
